6a422c61e383778724ebfd56ecd7708a1cd34944
RETBOT <robertoesquiveltr16@gmail.com>
2022-09-19 13:14:12 -0500

    Blog Pila completa 2

--- blog/tests.py
+++ blog/tests.py
@@ -34,15 +34,15 @@ class PruebasBlog(TestCase):
     
 
     def test_vista_lista_publicaciones(self):
-        respuesta = self.cliente.get(reverse('inicio'))
-        self.assertEqual(respuesta.status_code,200)
-        self.assertContains(respuesta,'Muy buen contenido')
-        self.assertTemplateUsed(respuesta, 'inicio.hmtl')
+        respuesta = self.client.get(reverse('inicio'))
+        self.assertEqual(respuesta.status_code,200),
+        self.assertContains(respuesta,'Muy buen contenido'),
+        self.assertTemplateUsed(respuesta,'inicio.html')
 
     def test_vista_detalle_publicacion(self):
-        respuesta = self.client.get('/pub/1')
-        sin_respuesta = self.client.get('/pub/100000/')
-        self.assertEqual(respuesta.status_code,200)
-        self.assertEqual(sin_respuesta, 404)
+        respuesta = self.client.get('/pub/1/')
+        sin_respuesta = self.client.get('/pub/1000000/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertEqual(sin_respuesta.status_code, 404)
         self.assertContains(respuesta, 'Un buen titulo')
         self.assertTemplateUsed(respuesta, 'detalle_publicaciones.html')
